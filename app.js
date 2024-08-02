4e10bb1bce519964a3bf98a0b9f5d4c29812892c
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-02 18:23:16 +0200

    methods property

--- app.js
+++ app.js
@@ -5,5 +5,15 @@ const app = Vue.createApp({
       vueLink: "https://vuejs.org/",
     };
   },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return "Learn Vue!";
+      } else {
+        return "Master Vue!";
+      }
+    },
+  },
 });
 app.mount("#user-goal");
